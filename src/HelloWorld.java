e53cd94889891955820aed7557d0cc01473ba075
unknown <juan.solozabal@opendeusto.es>
2020-11-10 22:51:50 +0100

    PruebasHelloWorld #PHT-1 add work 1h development

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public void HelloWorld () {
         System.out.println("Hello World, I'm Juan and this is my trial for GitHub and YouTrack ");
+        System.out.println("Hello World, I'm Juan and this is my trial for GitHub and YouTrack ");
+        System.out.println("Hello World, I'm Juan and this is my trial for GitHub and YouTrack ");
     }
 }
